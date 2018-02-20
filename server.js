9c66380b852c5a736e3f6e97844f234b9c2f53e1
subhamstark <34246051+subhamstark@users.noreply.github.com>
2018-02-20 18:23:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res) {
+    res.send('Article one here.');
+});
+
+app.get('/article-two', function(req, res) {
+    res.send('Article two here.');
+});
+
+app.get('/article-three', function(req, res) {
+    res.send('Article three here.');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
